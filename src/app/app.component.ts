9867c67bd68163738bf42007a87a7b74d55c1829
poorna99122 <poornachandrarao99122@gmail.com>
2021-11-02 09:31:49 -0700

    component selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  selector: 'app-root',
+  selector: 'mw-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
